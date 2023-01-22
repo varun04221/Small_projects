86bcd80fced0f319397e0125e31a329d6cfb4a3f
varun04221 <122555159+varun04221@users.noreply.github.com>
2023-01-22 15:02:05 +0530

    Update music.py

--- music.py
+++ music.py
@@ -2,89 +2,93 @@
 
 import csv
 import requests
+from math import ceil
 
 api_key='fae375fabd85c09a46c4405b106b88dd'
 
 #getting details from User for importing details
-while True:
-    country=input("Please Enter the Name of Your Country[REQUIRED]:")
-    country.lower()
-    country=country.replace(" ","_")
-    if requests.get("https://restcountries.com/v2/name/"+country).status_code==200:
-        break
-    else:
-        print("Please Enter A Valid Country Name!!!")
-singer=input("Please Enter the name of a artist or bypass it using enter/return:")
-singer.lower()
-
+sing=input("Please Enter the name of a singer or bypass it using enter/return:")
+sing.lower()
+fill=True
+if sing=="":
+    fill =False
+if not fill:
+    lyricist=""
+    while lyricist=="":
+        lyricist=input("Please Enter the name of a writer or bypass it using enter/return:")
+        if lyricist=="":
+            print("You have to provide the name of lyricist if you are not providing the name of the singer!!!")
+else:
+    lyricist=input("Please Enter the name of a writer or bypass it using enter/return:")
+singer=sing+" "+lyricist.lower()
 
 
 needof=True
+url='http://ws.audioscrobbler.com/2.0/?method=artist.search&artist={}&api_key={}&limit={}&format=json'.format(singer,api_key,10)
 
-#if user does not enter a artist name
-if singer=="":
-    print("You have not entered any artist so we are adding most played tracks as per your country.")
-    url='http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country={}&api_key={}&format=json'.format(country,api_key)
-
-
-    #if country exist in last fm data
-    if requests.get(url).status_code==200:
-        rdata=requests.get(url)
-        rdata=rdata.json()
-        header=["Name of the song","Artist","Link to play song","Play Count"]
-        rows=[]
-        for i in rdata["tracks"]["track"]:
-            l=[]
-            l.append(i["name"])
-            l.append(i["artist"]["name"])
-            l.append(i['url'])
-            l.append(i["listeners"])
-            rows.append(l)
-    #if country does not exist in last fm data
+#if artist exist in last fm data
+if requests.get(url).status_code==200 and len(requests.get(url).json()["results"]["artistmatches"]["artist"])>0:
+    rdata=requests.get(url)
+    rdata=rdata.json()
+    n=1
+    select_artist={}
+    for i in rdata["results"]["artistmatches"]["artist"]:
+        print(n,i["name"])
+        select_artist[n]=[i["mbid"],i['name']]
+        n+=1
+    
+    n=int(input("Please Enter the number corresponding to your Selected Artist[PLEASE SELECT FIRST OCCURENCE IF THERE ARE MULTIPLE ENTERIES WITH SIMILIAR NAME FOR BEST RESULTS]{If It does not work please try another option}:"))
+    mbid=select_artist[n][0]
+    name=select_artist[n][1]
+    #url to get tracks of artist when mbid is not specified
+    if mbid=="":
+        url='http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist={}&api_key={}&format=json'.format(name,api_key,500)
     else:
-        print("Sorry!!! Country:{} does not exist in database".format(country))
-        needof=False
-
-#If user enters a artist name
-else:
-    url='http://ws.audioscrobbler.com/2.0/?method=artist.search&artist={}&api_key={}&format=json'.format(singer,api_key)
-
-    #if artist exist in last fm data
-    if requests.get(url).status_code==200 and len(requests.get(url).json()["results"]["artistmatches"]["artist"])>0:
-        rdata=requests.get(url)
-        rdata=rdata.json()
-        n=1
-        select_artist={}
-        for i in rdata["results"]["artistmatches"]["artist"]:
-            print(n,i["name"])
-            select_artist[n]=[i["mbid"],i['name']]
-            n+=1
-        
-        n=int(input("Please Enter the number corresponding to your Selected Artist[PLEASE SELECT FIRST OCCURENCE IF THERE ARE MULTIPLE ENTERIES WITH SIMILIAR NAME FOR BEST RESULTS]{If It does not work please try another option}:"))
-        mbid=select_artist[n][0]
-        name=select_artist[n][1]
-        #url to get tracks of artist when mbid is not specified
-        if mbid=="":
-            url='http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist={}&api_key={}&format=json'.format(name,api_key)
+        url='http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist={}&mbid={}&api_key={}&format=json'.format(name,mbid,api_key,500)
+    #proceeding forward with the details of artist
+    if "toptracks" in requests.get(url).json():
+        pdata=requests.get(url)
+        pdata=pdata.json()
+        total=int(pdata["toptracks"]["@attr"]["total"])
+        if total<=1000:
+            pages=1
         else:
-            url='http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist={}&mbid={}&api_key={}&format=json'.format(name,mbid,api_key)
-        #proceeding forward with the details of artist
-        rdata=requests.get(url)
-        rdata=rdata.json()
-        header=["Name of the song","Artist","Link to play song","Play Count"]
+            pages=ceil(total/1000)
         rows=[]
-        for i in rdata["toptracks"]["track"]:
-            l=[]
-            l.append(i["name"])
-            l.append(name)
-            l.append(i["url"])
-            l.append(i["playcount"])
-            rows.append(l)
-        
-    #if artist not in last fm data
+        header=["Name of the song","Singer","Lyricist/Writer","Link to play song","Play Count"]
+        print("Please Wait...")
+
+        for i in range(1,pages+1):
+            if mbid=="":
+                url='http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist={}&api_key={}&limit={}&page={}&format=json'.format(name,api_key,1000,i)
+            else:
+                url='http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist={}&mbid={}&api_key={}&limit={}&page={}&format=json'.format(name,mbid,api_key,1000,i)
+            
+
+            rdata=requests.get(url)
+            rdata=rdata.json()
+            
+            for i in rdata["toptracks"]["track"]:
+                l=[]
+                l.append(i["name"])
+                if sing!="":
+                    l.append(sing)
+                else:
+                    l.append("Not Found")
+                if lyricist!="":
+                    l.append(lyricist)
+                else:
+                    l.append("Not Found")
+                l.append(i["url"])
+                l.append(i["playcount"])
+                rows.append(l)
     else:
-        print("Sorry No Matching Results...")
+        print("Sorry No songs found")
         needof=False
+#if artist not in last fm data
+else:
+    print("Sorry No Matching Results...")
+    needof=False
 
 #Working with CSV File
 if needof:
